4f84a5a7f1105fa14992c1cff917da51969fff24
haena Kang <rkdgosk127@gmail.com>
2024-05-02 09:59:41 +0900

    deploy build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+// cli가 이 파일을 읽어서 build를 할 때 참고하게 됨
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-practice" : "/",
+  outputDir: "docs",
+};
